96131532516b7e651b112c48852b5200b6f1ebe3
VAN QUAN <longhoangle@vans-macbook-air.local>
2016-03-29 15:32:19 +0700

    Initial Commit

---
+++ HocVui/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HocVui
+//
+//  Created by lehoanglong on 3/29/16.
+//  Copyright © 2016 LeHoangLong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
